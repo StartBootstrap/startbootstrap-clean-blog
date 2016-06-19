6d747aee161b725ae084bf4a64da970bec1e77c3
David Miller <hello@davidmiller.io>
2016-06-19 18:13:32 -0400

    fixed a typo in contact form script

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -8,7 +8,7 @@
 
 $(function() {
 
-    $("#contactFrom input,#contactForm textarea").jqBootstrapValidation({
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
