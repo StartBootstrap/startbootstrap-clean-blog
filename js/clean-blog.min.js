29e0b1ab7efbc3abfa2c2051ce91118b9259da7c
David Miller <hello@davidmiller.io>
2016-08-18 21:25:31 -0400

    version cleanup

--- js/clean-blog.min.js
+++ js/clean-blog.min.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Clean Blog v1.1.1 (http://startbootstrap.com/template-overviews/clean-blog)
+ * Start Bootstrap - Clean Blog v3.3.7 (http://startbootstrap.com/template-overviews/clean-blog)
  * Copyright 2013-2016 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
  */
