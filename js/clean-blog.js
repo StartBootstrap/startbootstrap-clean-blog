e28e85362928ee61e685373882e143f267e4c8fa
Patrick Kubiak <patrick@antuple.net>
2016-12-02 12:54:22 -0500

    mobile navbar fixed to top on scroll

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -13,28 +13,46 @@ $(function() {
 jQuery(document).ready(function($) {
     var MQL = 1170;
 
+    // make the navbar fixed
+    var fixNabar = function() {
+      $('.navbar-custom').css('position', 'fixed');
+      $('.navbar-custom').addClass('is-fixed');
+    };
+
     //primary navigation slide-in effect
-    if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height();
-        $(window).on('scroll', {
-                previousTop: 0
-            },
-            function() {
-                var currentTop = $(window).scrollTop();
-                //check if user is scrolling up
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
-                } else if (currentTop > this.previousTop) {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+    var headerHeight = $('.navbar-custom').height();
+    $(window).on('resize', function() {
+      if ($(window).width() > MQL && $(window).scrollTop() > 0) {
+        // fixed navbar on mobile
+        fixNabar();
+      }
+    });
+
+    $(window).on('scroll', {
+            previousTop: 0
+        },
+        function() {
+          if ($(window).width() > MQL) {
+            var currentTop = $(window).scrollTop();
+            //check if user is scrolling up
+            if (currentTop < this.previousTop) {
+                //if scrolling up...
+                if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                    $('.navbar-custom').addClass('is-visible');
+                } else {
+                    $('.navbar-custom').removeClass('is-visible is-fixed');
                 }
-                this.previousTop = currentTop;
-            });
-    }
+            } else if (currentTop > this.previousTop) {
+                //if scrolling down...
+                $('.navbar-custom').removeClass('is-visible');
+                if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
+                  $('.navbar-custom').addClass('is-fixed');
+                }
+            }
+          } else {
+            // fixed navbar on mobile
+            fixNabar();
+          }
+          this.previousTop = currentTop;
+        });
 });
