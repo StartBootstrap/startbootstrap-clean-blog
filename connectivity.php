a439023e3a3acc0e42bfb4341c4d5bf3966b3969
Daniel <whiteside.danielj@gmail.com>
2019-03-04 04:20:17 -0600

    Update connectivity.php

--- connectivity.php
+++ connectivity.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'login');
 define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_PASSWORD','36600dIg$');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
