d5ec92ed774f217f465a3dd44f3ad67d5a08d490
Daniel <whiteside.danielj@gmail.com>
2019-03-04 04:01:00 -0600

    Update connectivity.php

--- connectivity.php
+++ connectivity.php
@@ -6,10 +6,10 @@ define('DB_PASSWORD','');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
-/*
+
 $ID = $_POST['user'];
 $Password = $_POST['pass'];
-*/
+
 function SignIn()
 {
 session_start();   //starting the session for user profile page
