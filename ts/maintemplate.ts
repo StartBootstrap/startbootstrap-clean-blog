eeacce20880c72dd111612a8b5e8c6e2fa07064f
Stefan Sobek <s.sobek@fontys.nl>
2019-04-10 12:50:46 +0200

    added metatags

--- ts/maintemplate.ts
+++ ts/maintemplate.ts
@@ -275,9 +275,54 @@ lib.contentElement {
    }
 }
 
-
-page.meta.description.data = page:description
+# metatags
 page.meta.viewport  = width=device-width, initial-scale=1, shrink-to-fit=no
+
+page {
+	meta {
+		# Application Name - Um welche Anwendung handelt es sich?
+		application-name = Webpage
+		# Keywords - auslesen und ggf. vererben
+		keywords.data = page:keywords
+		keywords.ifEmpty.data = levelfield :-1, keywords, slide
+		keywords.keywords = 1
+		# Description - auslesen und ggf. vererben
+		description.data = page:description
+		description.ifEmpty.data = levelfield :-1, description, slide
+		# Author - auslesen und ggf. vererben
+		author.data = page:author
+		author.ifEmpty.data = levelfield :-1, author, slide
+		# Author Email - auslesen und ggf. vererben
+		author_email.data = page:author_email
+		author_email.ifEmpty.data = levelfield :-1, author_email, slide
+		# Abstract - auslesen und ggf. vererben
+		abstract.data = page:abstract
+		abstract.ifEmpty.data = levelfield :-1, abstract, slide
+		# Publisher/ Author - auslesen und ggf. vererben
+		publisher.data = page:author
+		publisher.ifEmpty.data = levelfield :-1, author, slide
+		# Copyright - Wer ist Urheber dieser Seite?
+		copyright = www.rhenaniahinsbeck.de
+		# Language - In welcher Sprache ist diese Seite?
+		language.data = TSFE:lang
+		# Robots - Such-Robots sollen den Links folgen und diese indizieren.
+		robots = index, follow
+		# Revisit After - Nach frühestens sollen die Such-Robots wieder kommen.
+		revisit-after = 7 days
+	}
+	# Meta-Tag Last-Modified, aus dem Datensatz der Seite auslesen.
+	# Alternativ kann auch das Feld last_update ausgelesen werden.
+	headerData {
+		1 = TEXT
+		1 {
+			data = page:SYS_LASTCHANGED
+			date = c
+			wrap = 
+		}
+	}
+}
+
+
 page.headerData >
 page.headerData = COA
 page.headerData.10 = TEXT
