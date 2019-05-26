2366c3fd53effd473bd59917dd605ba31f2364a2
Maegan <maegan.jong@gmail.com>
2019-05-26 16:49:31 -0500

    Edit Nav and fix borders

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("mainNav").innerHTML =
+ '<div class="container">'+
+
+
+   '<a class="navbar-brand" href="index.html">Maegan Jong</a>' +
+    '<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">' +
+    'Menu'+
+     '<i class="fas fa-bars"></i>'+
+   '</button>'+
+   '<div class="collapse navbar-collapse" id="navbarResponsive">'+
+     '<ul class="navbar-nav ml-auto">'+
+
+       '<li class="nav-item dropdown">'+
+         '<a class="nav-link" href="post.html" >Writing</a>'+
+       '</li>'+
+
+
+       '<li class="nav-item"> <a class="nav-link" href="about.html">About</a></li></ul></div></div>' ;
