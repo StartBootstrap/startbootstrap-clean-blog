136beb1bdde9caa6defaaf92b07bed19487abcfc
Maegan <maegan.jong@gmail.com>
2019-05-26 23:16:55 -0500

    footer

--- js/script.js
+++ js/script.js
@@ -20,3 +20,45 @@ document.getElementById("mainNav").innerHTML =
 
 
        '<li class="nav-item"> <a class="nav-link" href="about.html">About</a></li></ul></div></div>' ;
+
+document.getElementById("footer").innerHTML =
+'<div class="container">'+
+  '<div class="row">'+
+  '  <div class="col-lg-8 col-md-10 mx-auto">'+
+      '<ul class="list-inline text-center">'+
+      '  <li class="list-inline-item">'+
+          '<a href="mailto:maegan.jong@gmail.com">'+
+            '<span class="fa-stack fa-lg">'+
+              '<i class="fas fa-circle fa-stack-2x"></i>'+
+              '<i class="fa fa-envelope-square fa-stack-1x fa-inverse"></i>'+
+            '</span>'+
+          '</a>'+
+        '</li>'+
+        '<li class="list-inline-item">'+
+          '<a href="https://www.facebook.com/maeganjong">'+
+          '  <span class="fa-stack fa-lg">'+
+          '    <i class="fas fa-circle fa-stack-2x"></i>'+
+          '    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>'+
+          '  </span>'+
+          '</a>'+
+        '</li>'+
+        '<li class="list-inline-item">'+
+        '  <a href="https://github.com/maeganjong">'+
+        '    <span class="fa-stack fa-lg">'+
+        '      <i class="fas fa-circle fa-stack-2x"></i>'+
+        '      <i class="fab fa-github fa-stack-1x fa-inverse"></i>'+
+        '    </span>'+
+          '</a>'+
+        '</li>'+
+        '<li class="list-inline-item">'+
+        '  <a href="https://www.linkedin.com/in/maegan-jong/">'+
+        '    <span class="fa-stack fa-lg">'+
+        '      <i class="fas fa-circle fa-stack-2x"></i>'+
+        '      <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>'+
+        '    </span>'+
+          '</a>'+
+        '</li>'+
+      '</ul>'+
+      '</div>'+
+  '</div>'+
+'</div>';
