0d2447cd43bcece845faac4e63ff2f7a71e94ca2
Sam Artioli <initplatform@gmail.com>
2021-05-06 16:56:51 -0400

    scrollup effect on mainNav in pure js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,32 +1,22 @@
-// (function ($) {
-//     "use strict"; // Start of use strict
-
-//     // Show the navbar when the page is scrolled up
-//     var MQL = 992;
-
-//     //primary navigation slide-in effect
-//     if ($(window).width() > MQL) {
-//         var headerHeight = $('#mainNav').height();
-//         $(window).on('scroll', {
-//                 previousTop: 0
-//             },
-//             function () {
-//                 var currentTop = $(window).scrollTop();
-//                 //check if user is scrolling up
-//                 if (currentTop < this.previousTop) {
-//                     //if scrolling up...
-//                     if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
-//                         $('#mainNav').addClass('is-visible');
-//                     } else {
-//                         $('#mainNav').removeClass('is-visible is-fixed');
-//                     }
-//                 } else if (currentTop > this.previousTop) {
-//                     //if scrolling down...
-//                     $('#mainNav').removeClass('is-visible');
-//                     if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
-//                 }
-//                 this.previousTop = currentTop;
-//             });
-//     }
-
-// })(jQuery); // End of use strict
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function() {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if ( currentTop < scrollPos) {
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+})
