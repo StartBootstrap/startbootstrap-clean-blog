dffa8dbaec0bd15f9a5bbf73b9eb861161a487e8
David Miller <hello@davidmiller.io>
2021-05-13 16:51:34 -0400

    BS 5.0.1 and dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', function() {
         const currentTop = document.body.getBoundingClientRect().top * -1;
         if ( currentTop < scrollPos) {
+            // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
             } else {
@@ -17,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 mainNav.classList.remove('is-visible', 'is-fixed');
             }
         } else {
+            // Scrolling Down
             mainNav.classList.remove(['is-visible']);
             if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-fixed');
