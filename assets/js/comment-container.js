76529b9de4b6f2d145791cf73f88c70da67e7b94
Riley Grigg <griggriley@gmail.com>
2023-03-31 02:22:17 -0600

    test 2

--- assets/js/comment-container.js
+++ assets/js/comment-container.js
@@ -1,39 +1,18 @@
-const container = document.getElementById('container');
-const threshold = 50;
-let startY = 0;
-let endY = 0;
-let isRefreshing = false;
+var latestPosition = 0;
 
-window.addEventListener('touchstart', (event) => {
-  if (event.touches[0].clientY > window.innerHeight - threshold) {
-    startY = event.touches[0].clientY;
-    isRefreshing = false;
-  }
-});
-
-window.addEventListener('touchmove', (event) => {
-  if (isRefreshing) {
-    return;
-  }
-  
-  if (event.changedTouches[0].clientY > window.innerHeight - threshold) {
-    endY = event.changedTouches[0].clientY;
-    const distance = startY - endY;
-
-    if (distance > threshold) {
-      isRefreshing = true;
-      container.style.display = 'block';
-      // Call your refresh function here
-      console.log('Refresh triggered!');
+$(window).scroll(function () {
+    var $extra = $('#extra');
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        if ($extra.is(':visible')) {
+            alert('time to reload');
+        } else {
+            setTimeout(function () {
+                $extra.show();
+            }, 300)
+        }
     }
-  }
-});
-
-window.addEventListener('touchend', () => {
-  if (!isRefreshing) {
-    return;
-  }
-  
-  isRefreshing = false;
-  container.style.display = 'none';
-});
+    // console.log($(window).scrollTop() + " " + latestPosition)
+    if ($(window).scrollTop() < latestPosition)
+        $extra.hide();
+    latestPosition = $(window).scrollTop();
+})
