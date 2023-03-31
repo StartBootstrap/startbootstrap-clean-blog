56236a2db879e30f30dc682d507eaa09d8fb2459
Riley Grigg <griggriley@gmail.com>
2023-03-31 00:17:20 -0600

    adding functionality with JavaScript

---
+++ assets/js/accordion.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
