d5b70a61926c302fd1c9e3390622991128947abe
Riley Grigg <griggriley@gmail.com>
2023-03-31 01:20:31 -0600

    more bug fixes

--- assets/js/comment-container.js
+++ assets/js/comment-container.js
@@ -1,11 +1,14 @@
+// Get a reference to the container
+var comment-container = document.getElementById('comment-container');
+
 document.addEventListener('keypress', function (event) {
     // Check if the key pressed is the 'c' key
     if (event.key === 'c') {
-        // Toggle the visibility of the accordion
-        if (accordion.style.display === 'none') {
-            accordion.style.display = 'block';
+        // Toggle the visibility of the comment-container
+        if (comment-container.style.display === 'none') {
+            comment-container.style.display = 'block';
         } else {
-            accordion.style.display = 'none';
+            comment-container.style.display = 'none';
         }
     }
 });
