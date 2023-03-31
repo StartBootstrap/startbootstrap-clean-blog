2fc5b40fd64480cf66bdcdf6f3a1dd7d69bfd9a1
Riley Grigg <griggriley@gmail.com>
2023-03-31 01:51:51 -0600

    more testing

--- assets/js/comment-container.js
+++ assets/js/comment-container.js
@@ -1,65 +1,34 @@
 if (window.matchMedia("(max-width: 768px)").matches) {
-    // Get a reference to the container
-    var container = document.getElementById('container');
-  
-    // Add touch event listeners to the document
-    document.addEventListener('touchstart', handleTouchStart, false);
-    document.addEventListener('touchmove', handleTouchMove, false);
-  
-    // Variables to track swipe gesture
-    var xDown = null;
-    var yDown = null;
-  
-    // Handle touch start event
-    function handleTouchStart(event) {
-      xDown = event.touches[0].clientX;
-      yDown = event.touches[0].clientY;
-    }
-  
-    // Handle touch move event
-    function handleTouchMove(event) {
-      if (!xDown || !yDown) {
-        return;
-      }
-  
-      var xUp = event.touches[0].clientX;
-      var yUp = event.touches[0].clientY;
-  
-      var xDiff = xDown - xUp;
-      var yDiff = yDown - yUp;
-  
-      // Check if swipe is vertical and long enough
-      if (Math.abs(yDiff) > Math.abs(xDiff) && Math.abs(yDiff) > 100) {
-        // Swipe up to show the container
-        if (yDiff > 0 && container.style.display === 'none') {
-          container.style.display = 'block';
-          container.classList.add('show');
-        }
-        // Swipe down to hide the container
-        else if (yDiff < 0 && container.style.display === 'block') {
-          container.classList.remove('show');
-          setTimeout(function () {
-            container.style.display = 'none';
-          }, 300);
-        }
-      }
-  
-      // Reset variables for next swipe gesture
-      xDown = null;
-      yDown = null;
-    }
-  } else {
-    document.addEventListener('keypress', function (event) {
-      // Check if the key pressed is the 'c' key
-      if (event.key === 'c') {
-        // Toggle the visibility of the comment-container
-        var container = document.getElementById('container');
-        if (container.style.display === 'none') {
-          container.style.display = 'block';
-        } else {
-          container.style.display = 'none';
-        }
-      }
+    const container = document.getElementById('container');
+    let startY = 0;
+    let endY = 0;
+
+    window.addEventListener('touchstart', (event) => {
+        startY = event.touches[0].clientY;
     });
-  }
-  
+
+    window.addEventListener('touchend', (event) => {
+        endY = event.changedTouches[0].clientY;
+        const distance = startY - endY;
+
+        if (distance > 50) {
+            container.style.display = 'block';
+        } else if (distance < -50) {
+            container.style.display = 'none';
+        }
+    });
+
+} else {
+    document.addEventListener('keypress', function (event) {
+        // Check if the key pressed is the 'c' key
+        if (event.key === 'c') {
+            // Toggle the visibility of the comment-container
+            var container = document.getElementById('container');
+            if (container.style.display === 'none') {
+                container.style.display = 'block';
+            } else {
+                container.style.display = 'none';
+            }
+        }
+    });
+}
