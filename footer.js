0d51e99c83f8f9cc57875925337052df283efd12
Riley Grigg <griggriley@gmail.com>
2023-06-10 21:09:44 -0600

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,2 +1,2 @@
 document.getElementById("foot01").innerHTML =
-"<a href='http://eepurl.com/is87fg' target='_blank'>Receive Emails</a> <br>" + "<p>2017 - " + new Date().getFullYear() + " Riley Grigg </p> "
+"<a href='http://eepurl.com/is87fg' target='_blank'>Receive Email Updates</a> <br>" + "<p>2017 - " + new Date().getFullYear() + " Riley Grigg </p> "
