b4039922a7c6f6cf5861b88916d628989b620962
Riley Grigg <griggriley@gmail.com>
2023-06-17 10:07:41 -0600

    Create quote-share.js
    Beginning the process of moving code from inline to linking to it

---
+++ assets/js/quote-share.js
@@ -0,0 +1,36 @@
+<script>
+               // sleeping so the quote has time to load. Once the quote loads, we can get the text and share it through the link.
+               // sleep time expects milliseconds
+               function sleep(time) {
+                  return new Promise((resolve) => setTimeout(resolve, time));
+               }
+
+               // Usage!
+               sleep(5000).then(() => {
+                  // Do something after the sleep!
+                  let text = document.getElementById("quote").textContent;
+
+                  const shareData = {
+                     title: 'Quote',
+                     subject: 'Quote',
+                     text: text,
+                     url: 'https://love-of-god-and-of-all-men.github.io'
+                  }
+
+                  const link = document.querySelector('#quote-share-link');
+                  const successMessage = document.querySelector('#quote-share-link');
+                  const shareLinkText = document.querySelector('#quote-share-link');
+
+                  shareLinkText.textContent = 'Share Quote'
+
+                  // Share must be triggered by "user activation"
+                  link.addEventListener('click', async () => {
+                     try {
+                        await navigator.share(shareData)
+                        successMessage.textContent = 'Shared successfully!'
+                     } catch (err) {
+                        successMessage.textContent = 'Error: ' + err
+                     }
+                  });
+               });
+            </script>
