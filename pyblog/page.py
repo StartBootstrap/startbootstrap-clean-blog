c5e521ab226930fe3f798a8095bca66e5b14cc76
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-07-31 00:11:56 +0530

    update: template injection

--- pyblog/page.py
+++ pyblog/page.py
@@ -3,13 +3,6 @@ import re
 import markdown2
 
 from .utils import check_image
-from .template_injection import (
-    inject_head,
-    inject_navbar,
-    inject_header,
-    inject_markdown_content,
-    inject_footer,
-)
 
 
 class Page:
@@ -28,17 +21,55 @@ class Page:
         with open(template_file_path, "r") as f:
             self.html_text = f.read()
 
+    def inject_head(self):
+        with open("templates/head.html", "r") as f:
+            head_text = f.read()
+        self.html_text = re.sub(r"\{\{\s*head\s*\}\}", head_text, self.html_text)
+
+    def inject_navbar(
+        self,
+        site_name: str,
+        page_slugs: list,
+        page_texts: list,
+    ):
+        with open("templates/navbar.html", "r") as f:
+            navbar_text = f.read()
+        self.html_text = re.sub(r"\{\{\s*navbar\s*\}\}", navbar_text, self.html_text)
+        self.html_text = re.sub(r"\{\s*site-name\s*\}", site_name, self.html_text)
+        navbar_list_tags = ""
+        for page_slug, page_text in zip(page_slugs, page_texts):
+            navbar_list_tags += f'<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{page_slug}.html">{page_text}</a></li>'
+        self.html_text = re.sub(
+            r"\{\s*navbar-list\s*\}", navbar_list_tags, self.html_text
+        )
+
+    def inject_header(self):
+        with open("templates/header.html", "r") as f:
+            header_text = f.read()
+        self.html_text = re.sub(r"\{\{\s*header\s*\}\}", header_text, self.html_text)
+
+    def inject_markdown_content(self):
+        with open("templates/md-content.html", "r") as f:
+            md_content_text = f.read()
+        self.html_text = re.sub(
+            r"\{\{\s*md-content\s*\}\}", md_content_text, self.html_text
+        )
+
+    def inject_footer(self):
+        with open("templates/footer.html", "r") as f:
+            footer_text = f.read()
+        self.html_text = re.sub(r"\{\{\s*footer\s*\}\}", footer_text, self.html_text)
+
     def inject_templates(self):
-        self.html_text = inject_head(self.html_text)
-        self.html_text = inject_navbar(
-            self.html_text,
+        self.inject_head()
+        self.inject_navbar(
             site_name=self.site_name,
             page_texts=self.page_texts,
             page_slugs=self.page_slugs,
         )
-        self.html_text = inject_header(self.html_text)
-        self.html_text = inject_markdown_content(self.html_text)
-        self.html_text = inject_footer(self.html_text)
+        self.inject_header()
+        self.inject_markdown_content()
+        self.inject_footer()
 
     def generate_html(self, publish_file_path: str):
         for key, value in self.metadata.items():
