0c0b0b09b890452deb9d861644f8c9abf3123d43
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-07-30 21:14:34 +0530

    update: refactor pyblog

--- pyblog/generate.py
+++ pyblog/generate.py
@@ -2,20 +2,9 @@ import os
 import re
 import yaml
 import markdown2
-from PIL import Image
 
+from .utils import check_image
 
-def check_img(filename):
-    try:
-        im = Image.open(filename)
-        im.verify()
-        im.close()
-        im = Image.open(filename)
-        im.transpose(Image.FLIP_LEFT_RIGHT)
-        im.close()
-        return True
-    except Exception:
-        return False
 
 
 def inject_head(html_text: str):
@@ -59,7 +48,7 @@ def generate_index_html():
     html_text = inject_markdown_content(html_text)
     html_text = inject_footer(html_text)
     for key, value in metadata.items():
-        if check_img(os.path.join("./assets/images", value)):
+        if check_image(os.path.join("./assets/images", value)):
             value = os.path.join("../assets/images", value)
         elif os.path.isfile(os.path.join("./markdowns", value)):
             value = markdown2.markdown_path(os.path.join("./markdowns", value))
