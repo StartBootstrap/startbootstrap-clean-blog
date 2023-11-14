e59c06e98c90d248a18471b12be2bd5b603962a1
Riley Grigg <griggriley@gmail.com>
2023-11-14 11:10:27 -0700

    Revert "header and service worker updates. Service worker now shows offline page if page is not in cache and user is offline"
    This reverts commit 6e9b8bd9540efcab449175f29190fd860cf3daa1.

--- sw.js
+++ sw.js
@@ -1,13 +1,8 @@
-// A copy of each page is stored in the cache as your visitors view them.
-// This allows a visitor to load any previously viewed page while they are offline.
-// This then adds the "offline page" that allows you to customize the message
-// and experience if the app is offline, and the page is not in the cache.
+// This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline-page";
+const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -15,42 +10,9 @@ self.addEventListener("message", (event) => {
   }
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
 );
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
